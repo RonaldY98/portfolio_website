4c082368288facf9c27df404effee291afae43c0
RonaldY98 <16tfry@queensu.ca>
2023-11-11 16:39:07 -0500

    Added extra comments to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,24 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  //This property defines where the application starts
+  entry: "./src/index.js",
+
+  //This property defines the file path and the file name which will be used for deploying the bundled file
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
   },
+
+  // Pathname resolutions
   resolve: {
     modules: [path.join(__dirname, "src"), "node_modules"],
     alias: {
       react: path.join(__dirname, "node_modules", "react"),
     },
   },
+
+  // Setup loaders
   module: {
     rules: [
       {
@@ -34,6 +42,8 @@ module.exports = {
       },
     ],
   },
+
+  // Setup plugin to use a HTML file for serving bundled js files
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
